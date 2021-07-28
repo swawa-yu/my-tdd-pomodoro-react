7e1db44e4dc487bcdabe0ef0331e10dcfc783a8f
swawa-yu <swawa.yu@gmail.com>
2021-07-28 23:10:42 +0900

    25:00の表示

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("初期表示", () => {
+  test("「25:00」が描画されていること", () => {
+    const { getByTestId } = render(<App />);
+    expect(getByTestId("timeLeft").textContent).toEqual("25:00");
+  });
+  test.todo("「開始」が描画されていること");
+  test.todo("「作業」が描画されていること");
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+
+/** タイマーの長さ */
+const TIMER_LENGTH = { work: 25 * 60, break: 5 * 60 } as const;
+type TIMER_LENGTH = typeof TIMER_LENGTH[keyof typeof TIMER_LENGTH];
+
+interface State {
+  timeLeft: number;
+}
+
+/**
+ * 秒の数値をMM:SS形式の文字列に変換します。
+ * @param {number} second 秒
+ * @returns MM:SS形式の文字列
+ */
+const secondToMMSS = (second: number) => {
+  const MM =
+    second >= 10 * 60
+      ? Math.floor(second / 60).toString()
+      : second >= 1 * 60
+        ? "0" + Math.floor(second / 60).toString()
+        : "00";
+  const SS = second % 60 >= 10 ? second % 60 : "0" + (second % 60);
+  return MM + ":" + SS;
+};
+
+const App: React.VFC = () => {
+  const [state, setState] = useState<State>({
+    timeLeft: TIMER_LENGTH.work,
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div data-testid="timeLeft">25:00</div>
     </div>
-  );
+  )
 }
 
 export default App;
