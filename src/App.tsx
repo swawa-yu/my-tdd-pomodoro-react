3c54d9fab6501483a5397a63763f2ea781cf208f
swawa-yu <swawa.yu@gmail.com>
2021-07-31 16:14:02 +0900

    secondToMMSSを分離

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,5 @@
 import React, { useEffect, useState } from 'react';
-
-/**
- * 秒の数値をMM:SS形式の文字列に変換します。
- * @param {number} second 秒
- * @returns MM:SS形式の文字列
- */
-const secondToMMSS = (second: number) => {
-  const MM =
-    second >= 10 * 60
-      ? Math.floor(second / 60).toString()
-      : second >= 1 * 60
-        ? "0" + Math.floor(second / 60).toString()
-        : "00";
-  const SS = second % 60 >= 10 ? second % 60 : "0" + (second % 60);
-  return MM + ":" + SS;
-};
-
-
+import { secondToMMSS } from './util'
 
 
 
